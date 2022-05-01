8eb9fe4a914de49c1bdc803d4e283b7e57f47be4
mydream8 <101339846+mydream8@users.noreply.github.com>
2022-05-01 17:50:26 +0800

    Add files via upload

---
+++ 浮点型计算器2.2/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int main()
+{
+    double number_a=10.5,number_b=10.5;
+
+    char xuanxiang;
+
+    /*printf("输入1-4的一个数 1为加法 2为减法 3为乘法 4为除法\n");
+
+    scanf("%d",&xuanxiang);*/
+
+for(;;)
+{
+       printf("请输入要用的算式:>");
+
+//    printf("Please fuck enter the damn two numbers with your fucking shit like hands, separated by a space\n");
+
+    scanf("%lf%c%lf",&number_a,&xuanxiang,&number_b);
+
+
+if  (xuanxiang=='+')
+{
+  printf("%#lf\n",number_a+number_b);
+
+}
+
+else if(xuanxiang=='-')
+
+{
+  printf("%#lf\n",number_a-number_b);
+
+}
+
+else if(xuanxiang=='*')
+{
+   printf("%#lf\n",number_a*number_b);
+
+
+}
+
+else if(xuanxiang=='/')
+
+      {
+        if(number_b==0)
+
+          printf("除数不能为零\n");
+         else if(number_b!=0)
+        printf("%#lf\n",number_a/number_b);
+
+      }
+
+else
+{
+       printf("输入错误\n");
+}
+}
+
+
+    return 0;
+}
